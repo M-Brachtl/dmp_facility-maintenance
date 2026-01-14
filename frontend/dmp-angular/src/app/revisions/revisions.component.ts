273c39dcf343491826781f341908cdba49e351c8
Matej Brachtl <bra42176@spseol.cz>
2026-01-14 11:02:31 +0100

    Periodicity - list

--- frontend/dmp-angular/src/app/revisions/revisions.component.ts
+++ frontend/dmp-angular/src/app/revisions/revisions.component.ts
@@ -2,15 +2,32 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ModeService } from '../mode.service';
 import { HeaderBtnsComponent } from '../header-btns/header-btns.component';
+import { FormsModule } from '@angular/forms';
+import { DialogContainerComponent } from '../dialog-container/dialog-container.component';
+
+import { filterInterface } from '../filterInterface';
+
+declare const eel: any;
 
 @Component({
   selector: 'app-revisions',
-  imports: [HeaderBtnsComponent],
+  imports: [HeaderBtnsComponent, DialogContainerComponent, FormsModule],
   templateUrl: './revisions.component.html',
   styleUrl: './revisions.component.scss'
 })
 export class RevisionsComponent {
   mode!: 'list' | 'add' | 'remove';
+  filterI = new filterInterface();
+  revTypesList: any[] = [];
+  eel_on: boolean = false; // bez eel jsou použitá testovací data přímo v kódu
+  showDialog: boolean = false;
+  dialogContent: string = '';
+  machinesList: any[] = [];
+  inNumList: string[] = [];
+  facilityRevTypesList: any[] = [];
+  periodicities: any[] = [];
+  toggleMachineColText: number = 0;
+
   constructor(private route: ActivatedRoute, private modeService: ModeService) {}
   ngOnInit() {
     this.route.url.subscribe(url => {
@@ -18,7 +35,83 @@ export class RevisionsComponent {
     });
     this.modeService.mode$.subscribe(mode => {
       this.mode = mode;
+      this.filterI.hiddenStyleUpdate();
     });
+    this.getMachines();
+    this.getRevTypes();
+    this.getPeriodicities();
+
+    this.filterI.filterValues = {
+      machineName: '',
+      machineInNum: '',
+      revType: '',
+      minPeriod: '',
+      maxPeriod: ''
+    }
+  }
+
+  getMachines() {
+    if (!this.eel_on) {
+      this.machinesList = [ // ID, Inventory Number, Name, Type, Location, Location IDs, ...
+        [0, "FACILITY", "Facility", "Fictive - Facility", "No Location", [4], 0, []],
+        [1, "T_001", "Stroj A", "Test", "Lokace T", [1, 2], 0, []],
+        [2, "NM-001","New Test Machine vz.1", "Test", "New Testing Facility", [5, 3], 0, []],
+        [3, "NM-002","New Test Machine vz.2", "Test - A2", "Location - A2", [6], 0, []],
+        [4, "OLD-01","Old Machine", "Test", "No Location", [4], 1, [0,1,2,3]],
+        [5, "T_001", "Stroj A", "Test", "Lokace T", [1, 2], 0, []],
+        [6, "NM-001","New Test Machine vz.1", "Test", "New Testing Facility", [5, 3], 0, []],
+        [7, "NM-002","New Test Machine vz.2", "Test - A2", "Location - A2", [6], 0, []],
+        [8, "OLD-01","Old Machine", "Test", "No Location", [4], 1, [0,1,2,3]],
+        [9, "T_001", "Stroj A", "Test", "Lokace T", [1, 2], 0, []],
+        [10, "NM-001","New Test Machine vz.1", "Test", "New Testing Facility", [5, 3], 0, []],
+        [11, "NM-002","New Test Machine vz.2", "Test - A2", "Location - A2", [6], 0, []],
+        [12, "OLD-01","Old Machine", "Test", "No Location", [4], 1, [0,1,2,3]],
+        [13, "T_001", "Stroj A", "Test", "Lokace T", [1, 2], 0, []],
+        [14, "NM-001","New Test Machine vz.1", "Test", "New Testing Facility", [5, 3], 0, []],
+        [15, "NM-002","New Test Machine vz.2", "Test - A2", "Location - A2", [6], 0, []],
+        [16, "OLD-01","Old Machine", "Test", "No Location", [4], 1, [0,1,2,3]],
+        [17, "OLD-01","Old Machine", "Test", "No Location", [4], 1, [0,1,2,3]],
+      ];
+    } else {
+      eel.list_machines()().then((result: any) => {
+        console.log("Výsledek:", result);
+        this.machinesList = result;
+      });
+    }
+    this.inNumList = this.machinesList.map(machine => machine[1]);
+  }
+  getRevTypes() {
+    if (this.eel_on) {
+      eel.list_revision_types()().then((result: any) => {
+        console.log("Výsledek:", result);
+        this.revTypesList = result;
+      });
+      return;
+    } else {
+      this.revTypesList = [
+        [1, 'Revize T1', 12, 0],
+        [2, 'Revize T2', 24, 0],
+        [3, 'Revize T3-P', 48, 0],
+        [4, 'Revize F1-Test', 6, 1],
+        [5, 'Revize New_test 1', 60, 0]
+      ];
+    }
+    this.facilityRevTypesList = this.revTypesList.filter(rt => rt[3] == 1);
+  }
+
+  getPeriodicities() {
+    if (!this.eel_on){
+      this.periodicities = [[1, 1, 1, 12], [2, 2, 1, 24], [3, 5, 2, 6], [4, 3, 2, 60], [5, 4, 0, 18]];
+      return;
+    }
+  }
+
+  rowFiltered(entry: any[]){
+    return true;
+  }
+
+  isFacility(revTypeID: number){
+    return '';
   }
 
 }
